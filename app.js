8d3fc5b9d8da8ff0fb3843c180d7109c241b67aa
nileshoodles <nilesh.kumar@oodles.io>
2024-09-05 16:22:07 +0530

    auth route and post added to the app.js file

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 
 const app = express();
 
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/auth', authRoute);
+app.use('/api/post', postRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
