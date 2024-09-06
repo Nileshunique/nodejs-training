2cb64872aff420826b1ae3fbc5845c81fd89f46d
nileshoodles <nilesh.kumar@oodles.io>
2024-09-06 15:32:46 +0530

    post schema updated to change the type of userId and its relation to user document

--- schema/post.js
+++ schema/post.js
@@ -5,10 +5,9 @@ const blogSchema = new Schema({
   title: String, // String is shorthand for {type: String}
   delete: Boolean,
   blog: String,
-  userId: String,
+  userId: { type: mongoose.Types.ObjectId, ref: 'users', required: true },
   updatedAt: { type: Date, default: Date.now() },
   createdAt: { type: Date, default: Date.now },
-  // comments: [{ body: String, date: Date }],
 });
 
 const blogPost = new mongoose.model('Blog', blogSchema);
