bc60b047accc5f145fae8ab84163410bbcf35e78
nileshoodles <nilesh.kumar@oodles.io>
2024-09-06 14:32:56 +0530

    view and create comment route added to the post route.

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const { getPost, createPost, getPosts, updatePost, deletePost } = require('../controller/Post/post.controller');
+const { createComment, viewComment } = require('../controller/Post/comment.controller');
+
 var router = express.Router();
 
 router.get('/getPost', getPost);
@@ -8,5 +10,7 @@ router.post('/createPost', createPost);
 router.put('/updatePost', updatePost);
 router.delete('/deletePost', deletePost);
 
+router.get("/viewComment", viewComment);
+router.post("/createComment", createComment);
 
 module.exports = router;
