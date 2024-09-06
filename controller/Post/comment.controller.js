024c1ebc5d311611ea0d144ecfc56790333ce1d8
nileshoodles <nilesh.kumar@oodles.io>
2024-09-06 15:43:45 +0530

    try and catch block added to the post controller and comment controller.

--- controller/Post/comment.controller.js
+++ controller/Post/comment.controller.js
@@ -1,7 +1,7 @@
 const blogPost = require("../../schema/post");
 const commentDB = require("../../schema/postComment");
 const userdb = require("../../schema/userSchema");
-const { userNotFound, postNotFound, commentNotFound } = require("../../utils/errorList");
+const { userNotFound, postNotFound, commentNotFound, internalServerError } = require("../../utils/errorList");
 
 const createComment = async (req, res) => {
   // const { postId, userId, comment, parentId } = req.body;
@@ -17,54 +17,63 @@ const createComment = async (req, res) => {
 
   const { postId, userId, comment, parentId } = req.body;
 
-  // Use Promise.all to execute both queries concurrently
-  const [post, user, parentComment] = await Promise.all([
-    blogPost.findOne({ _id: postId }),
-    userdb.findOne({ _id: userId }),
-    parentId ? commentDB.findOne({ _id: parentId }) : null // Only check parentComment if parentId is provided
-  ]);
+  try {
+    // Use Promise.all to execute both queries concurrently
+    const [post, user, parentComment] = await Promise.all([
+      blogPost.findOne({ _id: postId }),
+      userdb.findOne({ _id: userId }),
+      parentId ? commentDB.findOne({ _id: parentId }) : null // Only check parentComment if parentId is provided
+    ]);
 
-  // Check if post and user exist before proceeding
-  if (!post) return postNotFound(res);
-  if (!user) return userNotFound(res);
-  if (parentId && !parentComment) return commentNotFound(res); // Check parentComment if parentId was provided
+    // Check if post and user exist before proceeding
+    if (!post) return postNotFound(res);
+    if (!user) return userNotFound(res);
+    if (parentId && !parentComment) return commentNotFound(res); // Check parentComment if parentId was provided
 
-
-  const reply = new commentDB({
-    comment,
-    userId,
-    postId,
-    parentComment: parentId || null,
-  });
-
-  await reply.save();
-
-  if (parentId) {
-    await commentDB.findByIdAndUpdate({ _id: parentId }, {
-      $push: { replies: reply._id }
+    const reply = new commentDB({
+      comment,
+      userId,
+      postId,
+      parentComment: parentId || null,
     });
-  }
 
-  res.status(200).json({ message: "Success", data: reply })
+    await reply.save();
+
+    if (parentId) {
+      await commentDB.findByIdAndUpdate({ _id: parentId }, {
+        $push: { replies: reply._id }
+      });
+    }
+
+    res.status(200).json({ message: "Success", data: reply })
+  } catch (err) {
+    console.error("Error in Create Comment controller", err.message);
+    return internalServerError(res)
+  }
 
 }
 
 const viewComment = async (req, res) => {
   const { postId } = req.query;
 
-  const post = await blogPost.findOne({ _id: postId })
-  if (!post) { return postNotFound(res); }
+  try {
+    const post = await blogPost.findOne({ _id: postId })
+    if (!post) { return postNotFound(res); }
 
-  const comments = await commentDB.find({ postId: postId, parentComment: null }) // Fetch top-level comments
-    .populate({
-      path: 'replies',
-      populate: {
-        path: 'replies', // Nested population for replies
-      },
-    });
+    const comments = await commentDB.find({ postId: postId, parentComment: null }) // Fetch top-level comments
+      .populate({
+        path: 'replies',
+        populate: {
+          path: 'replies', // Nested population for replies
+        },
+      });
 
-  res.status(200).json({ message: "Success", data: comments });
+    res.status(200).json({ message: "Success", data: comments });
 
+  } catch (err) {
+    console.error("Error in View Comments controller", err.message);
+    return internalServerError(res)
+  }
 };
 
 module.exports = {
--- controller/Post/post.controller.js
+++ controller/Post/post.controller.js
@@ -1,76 +1,103 @@
 const userdb = require("../../schema/userSchema");
-const { userNotFound } = require("../../utils/errorList");
+const { userNotFound, internalServerError } = require("../../utils/errorList");
 const blogPost = require("./../../schema/post")
 
 const getPost = async (req, res) => {
   const postId = req.query.postId;
-  if (postId) {
-    const post = await blogPost.find({ _id: postId, $or: [{ delete: false }, { delete: { $exists: false } }] });
-    res.status(200).json({ message: "Success", data: post });
-  } else {
-    const posts = await blogPost.find({ $or: [{ delete: false }, { delete: { $exists: false } }] }).sort({ updatedAt: -1 }).limit(2);
-    res.status(200).json({ message: "Success", data: posts });
+
+  try {
+    if (postId) {
+      const post = await blogPost.find({ _id: postId, $or: [{ delete: false }, { delete: { $exists: false } }] });
+      res.status(200).json({ message: "Success", data: post });
+    } else {
+      const posts = await blogPost.find({ $or: [{ delete: false }, { delete: { $exists: false } }] }).sort({ updatedAt: -1 }).limit(2);
+      res.status(200).json({ message: "Success", data: posts });
+    }
+  } catch (err) {
+    console.error("Error in get Post controller", err.message);
+    return internalServerError(res)
   }
 }
 
 const getPosts = async (req, res) => {
   const userId = req.query.userId;
-  if (!userId) {
-    res.status(403).json({ message: "User Id is required" })
+
+  try {
+    if (!userId) {
+      res.status(403).json({ message: "User Id is required" })
+    }
+
+    const user = await userdb.findOne({ _id: userId })
+    if (!user) { return userNotFound(res); }
+
+    const posts = await blogPost.find({ userId: userId, $or: [{ delete: false }, { delete: { $exists: false } }] });
+    res.status(200).json({ message: "Success", data: posts });
+  } catch (err) {
+    console.error("Error in Get Posts controller", err.message);
+    return internalServerError(res)
   }
-
-  const user = await userdb.findOne({ _id: userId })
-  if (!user) { return userNotFound(res); }
-
-  const posts = await blogPost.find({ userId: userId, $or: [{ delete: false }, { delete: { $exists: false } }] });
-  res.status(200).json({ message: "Success", data: posts });
 }
 
 const createPost = async (req, res) => {
   const { title, blog, userId } = req.body;
-  if (!title || !blog || !userId) {
-    res.status(403).json({ message: "Please enter all details" })
+  
+  try {
+    if (!title || !blog || !userId) {
+      res.status(403).json({ message: "Please enter all details" })
+    }
+
+    const user = await userdb.findOne({ _id: userId })
+    if (!user) { return userNotFound(res); }
+
+    const post = new blogPost({ title, blog, userId });
+    await post.save();
+    res.status(200).json({ message: "Success", data: post })
+  } catch (err) {
+    console.error("Error in Create Post controller", err.message);
+    return internalServerError(res)
   }
-
-  const user = await userdb.findOne({ _id: userId })
-  if (!user) { return userNotFound(res); }
-
-  const post = new blogPost({ title, blog, userId });
-  await post.save();
-  res.status(200).json({ message: "Success", data: post })
 }
 
 const updatePost = async (req, res) => {
   const { title, blog, postId, userId } = req.body;
 
-  if (!title || !blog || !postId || !userId) {
-    res.status(403).json({ message: "Please enter all details" })
+  try {
+    if (!title || !blog || !postId || !userId) {
+      res.status(403).json({ message: "Please enter all details" })
+    }
+
+    const user = await userdb.findOne({ _id: userId })
+    if (!user) { return userNotFound(res); }
+
+    // const post = new blogPost({ title, blog, userId });
+    const post = await blogPost.findOneAndUpdate({ _id: postId, userId: userId, $or: [{ delete: false }, { delete: { $exists: false } }] }, { title, blog }, { new: true })
+    // await post.save();
+    res.status(200).json({ message: "Success", data: post })
+  } catch (err) {
+    console.error("Error in Update Post controller", err.message);
+    return internalServerError(res)
   }
-
-  const user = await userdb.findOne({ _id: userId })
-  if (!user) { return userNotFound(res); }
-
-  // const post = new blogPost({ title, blog, userId });
-  const post = await blogPost.findOneAndUpdate({ _id: postId, userId: userId, $or: [{ delete: false }, { delete: { $exists: false } }] }, { title, blog }, { new: true })
-  // await post.save();
-  res.status(200).json({ message: "Success", data: post })
 
 }
 
 const deletePost = async (req, res) => {
   const { postId, userId } = req.body;
 
-  if (!postId || !userId) {
-    res.status(403).json({ message: "Please enter all detail." })
+  try {
+    if (!postId || !userId) {
+      res.status(403).json({ message: "Please enter all detail." })
+    }
+
+    const user = await userdb.findOne({ _id: userId })
+    if (!user) { return userNotFound(res); }
+
+    const post = await blogPost.findOneAndUpdate({ _id: postId, userId: userId }, { delete: true }, { new: true })
+    // await post.save();
+    res.status(200).json({ message: "Success", data: post })
+  } catch (err) {
+    console.error("Error in Delete Post controller", err.message);
+    return internalServerError(res)
   }
-
-  const user = await userdb.findOne({ _id: userId })
-  if (!user) { return userNotFound(res); }
-
-  const post = await blogPost.findOneAndUpdate({ _id: postId, userId: userId }, { delete: true }, { new: true })
-  // await post.save();
-  res.status(200).json({ message: "Success", data: post })
-
 }
 
 module.exports = {
